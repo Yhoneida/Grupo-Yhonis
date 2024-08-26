733c9cb9a1a182321a7f68a4ff2bb90e5815b7ef
linita <linita03118@gmail.com>
2024-08-26 07:28:04 -0500

    version1

---
+++ Valentina/valen.py
@@ -0,0 +1,8 @@
+aseo = set({'Clorox','Jabón','Suavitel','Fabuloso','Fab','Varsol','Limpiavidrios'})
+granel = set({'Arroz','Harina','Clorox','Lenteja','Café','Fab','Maíz'})
+              
+              
+a=aseo.intersection(granel)
+
+print(f"estos son los productos que se alcanzaron a pasara a otra area: \n {a}")
+              
